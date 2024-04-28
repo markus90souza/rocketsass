8060fd45f3d32e9ca38ba8f71d68180384ed642e
rockske8er <rockske8er@gmail.com>
2024-04-28 03:15:23 -0300

    feat: setup database

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 export const createAccount = async (app: FastifyInstance) => {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
@@ -13,6 +16,34 @@ export const createAccount = async (app: FastifyInstance) => {
         }),
       },
     },
-    async () => {},
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(409).send({
+          message: 'email_in_use: Email is already in use',
+        })
+      }
+
+      const hashedPassword = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      })
+
+      reply.status(201).send({
+        message: 'account_created: Account created successfully',
+      })
+    },
   )
 }
