d0e6b2dfc1bc19ad9fd3fb68ead29ce3f234799d
rockske8er <rockske8er@gmail.com>
2024-05-01 02:37:26 -0300

    feat: create recover and reset password routes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,6 +14,8 @@ import { errorHandler } from './error-handler'
 import { authenticateWithCredentials } from './routes/auth/authenticate-with-credentials'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
+import { requestPasswordRecover } from './routes/auth/request-password-recover'
+import { resetPassword } from './routes/auth/reset-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
@@ -31,16 +33,19 @@ app.register(fastifySwagger, {
   transform: jsonSchemaTransform,
 })
 
-app.register(fastifyCors)
 app.register(fastifySwaggerUI, { routePrefix: '/docs' })
 app.register(fastifyJwt, {
   secret: 'rocket-sass',
 })
 
+app.register(fastifyCors)
+
 // AUTH
 app.register(createAccount)
 app.register(authenticateWithCredentials)
 app.register(getProfile)
+app.register(requestPasswordRecover)
+app.register(resetPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
