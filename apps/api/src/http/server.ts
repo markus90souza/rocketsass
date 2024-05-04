dae81972d02e1a1e7ff9d7a21f1c41a788122ba1
rockske8er <rockske8er@gmail.com>
2024-05-03 22:34:53 -0300

    feat: add create organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createOrganization } from './routes/orgs/create-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
@@ -57,6 +58,8 @@ app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
+// ORGANIZATIONS
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
