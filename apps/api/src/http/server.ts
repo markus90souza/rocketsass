66ff2b1dc8ac3591abd7a9a49fc5379d6c157a51
rockske8er <rockske8er@gmail.com>
2024-05-06 09:16:37 -0300

    feat: create get all projects and unique project route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,8 @@ import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
 import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
+import { getProjects } from './routes/projects/get-projects'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
@@ -77,6 +79,8 @@ app.register(getMembership)
 // PROJECTS
 app.register(createProject)
 app.register(deleteProject)
+app.register(getProject)
+app.register(getProjects)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
